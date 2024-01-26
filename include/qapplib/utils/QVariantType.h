b1e223c4309c5c8be2a76f9d1554d3bf3fa11680
martin-fitger <57917288+martin-fitger@users.noreply.github.com>
2024-01-26 09:20:50 +0100

    More variant types.

--- include/qapplib/utils/QVariantType.h
+++ include/qapplib/utils/QVariantType.h
@@ -25,18 +25,37 @@ along with QAppLib. If not, see <http://www.gnu.org/licenses/>.
 
 namespace qapp
 {
+	class QVariantEx
+	{
+	public:
+		enum Type
+		{
+			Float = QVariant::UserType + 1,
+		};
+	};
+
 	template <typename T> QVariant::Type QVariantType();
 
 	template <> inline QVariant::Type QVariantType<int>()          { return QVariant::Int; }
 	template <> inline QVariant::Type QVariantType<unsigned int>() { return QVariant::UInt; }
+	template <> inline QVariant::Type QVariantType<float>()        { return (QVariant::Type)QVariantEx::Float; }
+	template <> inline QVariant::Type QVariantType<double>()       { return QVariant::Double; }
 	template <> inline QVariant::Type QVariantType<QPointF>()      { return QVariant::PointF; }
 
 	inline QString QVariantTypeToString(QVariant::Type type)
 	{
 		switch (type)
 		{
+		case QVariant::Int:
+			return QString("Int");
 		case QVariant::UInt:
 			return QString("UInt");
+		case (QVariant::Type)QVariantEx::Float:
+			return QString("Float");
+		case QVariant::Double:
+			return QString("Double");
+		case QVariant::String:
+			return QString("String");
 		case QVariant::PointF:
 			return QString("PointF");
 		}
@@ -45,8 +64,14 @@ namespace qapp
 
 	inline QVariant::Type QVariantTypeFromString(const QString& s)
 	{
+		if (s == "Int")
+			return QVariant::Int;
 		if (s == "UInt")
 			return QVariant::UInt;
+		if (s == "Float")
+			return (QVariant::Type)QVariantEx::Float;
+		if (s == "Double")
+			return QVariant::Double;
 		if (s == "String")
 			return QVariant::String;
 		if (s == "PointF")
@@ -58,8 +83,14 @@ namespace qapp
 	{
 		switch (type)
 		{
+		case QVariant::Int:
+			return sizeof(int32_t);
 		case QVariant::UInt:
 			return sizeof(uint32_t);
+		case (QVariant::Type)QVariantEx::Float:
+			return sizeof(float);
+		case QVariant::Double:
+			return sizeof(double);
 		case QVariant::PointF:
 			return sizeof(QPointF);
 		}
